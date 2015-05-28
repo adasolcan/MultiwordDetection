b3db502a88491affc27cc3c96c7f396ad681a59e
adasolcan <adasolcan@yahoo.com>
2015-05-28 15:59:02 +0300

    Using Twitter POS Tagging

--- multiwords.py
+++ multiwords.py
@@ -3,78 +3,65 @@ import collections
 import csv
 import subprocess
 
-inputFile = 'tweets_1.txt'
+input_file = 'tweets_trends.txt'
 
-p = subprocess.Popen(["../ark-tweet-nlp-0.3.2/runTagger.sh", "--no-confidence", "--output-format", "pretsv", "--quiet", inputFile], stdout=subprocess.PIPE)
+p = subprocess.Popen(["../ark-tweet-nlp-0.3.2/runTagger.sh", "--no-confidence", "--output-format", "pretsv", "--quiet", input_file], stdout=subprocess.PIPE)
 (output, err) = p.communicate()
-tokenList_temp = re.sub("\n", "\t", output).split("\t")
+token_list_temp = re.sub("\n", "\t", output.decode()).split("\t")
 
 i=0
-tokenList=[]
-while i<len(tokenList_temp):
-  tokenList.append(tokenList_temp[i:i+2])
+token_list=[]
+while i<len(token_list_temp):
+  token_list.append(token_list_temp[i:i+2])
   i+=3
 
-print tokenList
-
 words = ['']
-dictWord = collections.defaultdict(int)
-particles = collections.defaultdict(int)
-f = open('prepositions.txt', 'r')
-for prep in f:
-    particles[prep.rstrip()]+=1
-f.close()
-f = open('articles.txt', 'r')
-for article in f:
-    particles[article.rstrip()]+=1
-f.close()
+dict_word = collections.defaultdict(int)
 
 tweet1 = ''
-for group in tokenList:
+for group in token_list:
     if len(group) == 2:
         tweet = group[0]
         tag = group[1]
     if tweet1 != tweet:
         words = tweet.split()
         tags = tag.split()
-        print words
-        print tags
 
         word1 = ""
         word2 = ""
-        word3 = ""
         tag1 = ""
         tag2 = ""
-        tag3 = ""
         pair = ""
         for i in range(0, len(words)-1):
             word = words[i]
             tag = tags[i]
-            if word3!="" and word2!="" and word1!="" and word!="":
-                if not((word1 in particles) or (word in particles)):
+            if tag == "N" or tag == "^":
+                if tag1 in ["N", "^", "A", "V"]:
                     pair = word1 + " " + word
-                    dictWord[pair]+=1
-                if not(word2 in particles):
-                     pair = word2 + " " + word1 + " " + word
-                     dictWord[pair]+=1
-                if not(word3 in particles):
-                     pair = word1 + " " + word2 + " " + word3 + " " + word
-                     dictWord[pair]+=1
-            word1 = word
-            tag1 = tag
+                    dict_word[pair]+=1
+                if (tag1 in ["N", "^", "A"]) and (tag2 in ["N", "^", "A"]):
+                    pair = word2 + " " + word1 + " " + word
+                    dict_word[pair]+=1
+                if (tag1 in ["P"]) and (tag2 in ["N", "^"]):
+                    pair = word2 + " " + word1 + " " + word
+                    dict_word[pair]+=1
+            if tag == "V":
+                if tag1 in ["N", "^", "R"]:
+                    pair = word1 + " " + word
+                    dict_word[pair]+=1
             word2 = word1
             tag2 = tag1
-            word3 = word2
-            tag3 = tag2
+            word1 = word
+            tag1 = tag
     tweet1 = tweet
 
 f.close()
-# print(dictWord.items())
+# print(dict_word.items())
 
-orderedDictWord = collections.OrderedDict(sorted(dictWord.items(), key=lambda t: t[1], reverse=True))
-# print(orderedDictWord.items())
+ordered_dict_word = collections.OrderedDict(sorted(dict_word.items(), key=lambda t: t[1], reverse=True))
+# print(ordered_dict_word.items())
 
 w = csv.writer(open("dictionaryPair.csv", "w"))
-for key, val in orderedDictWord.items():
+for key, val in ordered_dict_word.items():
     if val > 1:
          w.writerow([key, val])
