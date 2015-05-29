93cb5e68247556c9225dab7a8cf85e9ee13a709e
adasolcan <adasolcan@yahoo.com>
2015-05-29 19:45:01 +0300

    added multiwords patterns

--- multiwords.py
+++ multiwords.py
@@ -12,11 +12,16 @@ token_list_temp = re.sub("\n", "\t", output.decode()).split("\t")
 i=0
 token_list=[]
 while i<len(token_list_temp):
-  token_list.append(token_list_temp[i:i+2])
-  i+=3
+    token_list.append(token_list_temp[i:i+2])
+    i+=3
 
 dict_multiword = collections.defaultdict(int)
 dict_word = collections.defaultdict(int)
+
+multiword_patterns = [["N", "N"], ["A", "S"], ["V", "N"], 
+                    ["N", "N", "N"], ["A", "N", "N"], ["N", "A", "N"], ["A", "A", "N"], ["N", "P", "N"], 
+                    ["N", "V"], ["R" , "V"], ["N", "P", "A", "N"], ["N", "P", "N", "N"],
+                    ["A", "N", "P", "N", "N", "N", "P", "N"]]
 
 tweet1 = ''
 for group in token_list:
@@ -38,20 +43,13 @@ for group in token_list:
             if tag in ["N", "A", "V", "R", "P"]:
                 dict_word[word] += 1
 
-            if tag == "N":
-                if tag1 in ["N", "A", "V"]:
-                    multiword = word1 + " " + word
-                    dict_multiword[multiword] += 3
-                if (tag1 in ["N", "A"]) and (tag2 in ["N", "A"]):
-                    multiword = word2 + " " + word1 + " " + word
-                    dict_multiword[multiword] += 4
-                if (tag1 in ["P"]) and (tag2 in ["N"]):
-                    multiword = word2 + " " + word1 + " " + word
-                    dict_multiword[multiword] += 4
-            if tag == "V":
-                if tag1 in ["N", "R"]:
-                    multiword = word1 + " " + word
-                    dict_multiword[multiword] += 3
+            if [tag1, tag] in multiword_patterns:
+                multiword = word1 + " " + word
+                dict_multiword[multiword] += 1
+            if [tag2, tag1, tag] in multiword_patterns:
+                multiword = word2 + " " + word1 + " " + word
+                dict_multiword[multiword] += 1
+
             word2 = word1
             tag2 = tag1
             word1 = word
@@ -61,8 +59,8 @@ for group in token_list:
 dict_multiword_score = collections.defaultdict(int)
 
 for key, val in dict_multiword.items():
-    dict_multiword_score[key] = val
     words = key.split()
+    dict_multiword_score[key] = val * (len(words) + 1)
     for word in words:
         dict_multiword_score[key] -= dict_word[word]
 
@@ -73,8 +71,8 @@ ordered_dict_multiword_score = collections.OrderedDict(sorted(dict_multiword_sco
 
 w = csv.writer(open("dictionaryMultiword.csv", "w"))
 for key, val in ordered_dict_multiword_score.items():
-    if val > 1:
-        w.writerow([key, val])
+    #if val > 1:
+    w.writerow([key, val])
 
 
 w = csv.writer(open("dictionaryWord.csv", "w"))
